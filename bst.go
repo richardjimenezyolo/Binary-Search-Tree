9a7a8db28b0fa59e701e4ca141df67942ddc857d
Richard <richardjimenez176@gmail.com>
2020-05-21 07:13:20 -0400

    First Commit

---
+++ bst.go
@@ -0,0 +1,82 @@
+package bst
+
+import (
+	"fmt"
+)
+
+// Node Structure
+
+type Node struct {
+	Left  *Node
+	val   int
+	Right *Node
+}
+
+// Insert number into the Tree
+func (t *Node) Insert(v int) error {
+	// If the Node is Empty
+	if t.val == 0 {
+		t.val = v
+	}
+	// Left
+	if t.val > v {
+		if t.Left == nil {
+			t.Left = &Node{val: v}
+			return nil
+		}
+
+		return t.Left.Insert(v)
+	}
+	// Right
+	if t.val < v {
+		if t.Right == nil {
+			t.Right = &Node{val: v}
+			return nil
+		}
+		return t.Right.Insert(v)
+	}
+	return nil
+}
+
+// Get The Smallest Number
+func (t *Node) GetMin() int {
+	if t.Left == nil {
+		return t.val
+	}
+	return t.Left.GetMin()
+}
+
+// Get The Biggest Number
+func (t *Node) GetMax() int {
+	if t.Right == nil {
+		return t.val
+	}
+
+	return t.Right.GetMax()
+}
+
+// Get A Certain Node
+func (t Node) GetNode(n int) int {
+	if t.val == n {
+		return n
+	}
+	if n > t.val {
+		fmt.Println("Right")
+		return t.Right.GetNode(n)
+	}
+	if n < t.val {
+		fmt.Println("Left")
+		return t.Left.GetNode(n)
+	}
+	return t.GetNode(n)
+}
+
+// Order The Tree
+func (this *Node) Order() {
+	if this == nil {
+		return
+	}
+	this.Left.Order()
+	fmt.Println(this.val)
+	this.Right.Order()
+}
